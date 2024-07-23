e3ed9876c00f356c9102dbb174fe414deadfa9cb
genku <koraygench@gmail.com>
2024-07-23 21:19:33 +0300

    first router

--- backend/app.js
+++ backend/app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const productRoute = require("./routes/products");
 
 require("dotenv").config();
 
@@ -6,6 +7,8 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
+
+app.use("/products", productRoute);
 
 app.listen(PORT, () => {
   console.log(`Storage app running on port ${PORT}`);
